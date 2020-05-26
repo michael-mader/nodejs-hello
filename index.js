ef573c26820844fb4f1bb127066e08e6bf11c927
Fender123 <Fender123@users.noreply.github.com>
2020-05-26 15:08:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const http = require('http')
 const port = 3000
 
 const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
+  console.log(request.url);
+  response.end('Hello Node.js Server! ' + new Date().toISOString());
+};
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log('something bad happened', err);
   }
 
-  console.log(`server is listening on ${port}`)
-})
+  console.log(`server is listening on ${port}`);
+});
